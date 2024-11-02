bafc780f39a816111c8fb93ea4cc8f49e5978836
Benjamin Venegas <benjaminvenegas014@gmail.com>
2024-11-01 22:11:14 -0300

    Agregacion del arreglo para el registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -4,6 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonicModule, NavController } from '@ionic/angular';
 
+import { StorageService } from 'src/app/storage.service';
+
+interface Persona{
+  username: string;
+  password: string;
+  userRole: string;
+  identificador: string;
+}
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -14,12 +23,21 @@ import { IonicModule, NavController } from '@ionic/angular';
 export class RegistroPage {
   
   
-  username!: string;
-  password!: string; 
-  userRole!: string;
- 
+  username:string='';
+  password:string=''; 
+  userRole:any='';
+
+  // Variables para leer parametros
+
+  par_username: string='';  
+  par_password: string='';  
+  
+  // Variables para el crud
+  personas:Persona[]=[];
+  currentId:string=""; 
 
   constructor(private router: Router,
+              private storageservice: StorageService,
               private navCtrl: NavController
   ) {
 
@@ -29,11 +47,24 @@ export class RegistroPage {
 
   
 
-  registro() {
-    console.log('Usuario registrado:', { username: this.username, role: this.userRole });
-    alert('Usuario registrado exitosamente');
-    this.router.navigate(['/login']);
-  }
+  async registro() {
+    const nuevoRegistro={
+      username:this.username,
+      password:this.password,
+      userRole:this.userRole,
+      identificador:Date.now().toString() //Genera un id
+    }
+    // Agregar a la nueva persona al arreglo 
+    this.personas.push(nuevoRegistro);
+
+    let resp = this.storageservice.registro('personas', nuevoRegistro);
+
+    // console.log('Usuario registrado:', { username: this.username, role: this.userRole });
+    // alert('Usuario registrado exitosamente');
+    // this.router.navigate(['/login']);
+  
+  
+  } // Fin de registro
 
 
     volver() {
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+
 import { importProvidersFrom } from '@angular/core';
 import { IonicStorageModule } from '@ionic/storage-angular';
+
+
 
 bootstrapApplication(AppComponent, {
   providers: [
